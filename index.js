3f73ed01759518281ca547774e33d71321edcfd8
Shreyas Karanam <69723018+k22shreyas@users.noreply.github.com>
2023-01-18 11:27:56 +0000

    search items using api fetch

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-const express = require('express')
+import fetch from 'node-fetch';
+import express, { response } from 'express';
 const app = express()
 const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+app.get("/", function (request, response){
+  response.sendFile("/workspaces/codespaces-express/index.html");
+});
+
+app.get("/getsearchword", function (request, response){
+  var searchword = request.query.first;
+  if (searchword != "") {
+    console.log(searchword);
+    getSearchWord(searchword);
+  }else{
+    response.send("Input the search word");
+  }
+});
+
+async function getSearchWord(searchword){
+  let requestOptions = {};
+  await fetch(`https://api.apilayer.com/spoonacular/recipes/autocomplete?query=${searchword}&apikey=mGAW74xIyucj3kfBlC996NK7rw1pXg06`, requestOptions)
+  .then(response => response.json())
+  .then((data) =>{
+    data.forEach(obj => {
+      Object.entries(obj).forEach(([key, value]) => {
+          console.log(`${key} ${value}`);
+      });
+      console.log('-------------------');
+    });
+  })
+  .catch((error) => {
+    console.log(error)
+  });
+  
+}
