fbbc7dfee21e3ba23d839cef220654c289f0d803
Shreyas Karanam <69723018+k22shreyas@users.noreply.github.com>
2023-01-30 10:38:29 +0000

    Pending changes exported from your codespace

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import fetch from 'node-fetch';
 import express, { response } from 'express';
+import https from 'https';
 const app = express()
 const port = 3000
 
@@ -15,26 +16,41 @@ app.get("/getsearchword", function (request, response){
   var searchword = request.query.first;
   if (searchword != "") {
     console.log(searchword);
-    getSearchWord(searchword);
+    const url = `https://api.apilayer.com/spoonacular/recipes/autocomplete?query=${searchword}&apikey=mGAW74xIyucj3kfBlC996NK7rw1pXg06`
+    https.get(url,function(response){
+      response.on("data",function(data){
+        const items =[];
+        data.forEach(obj => {
+          //obj now points to each entry in the data variable
+          items.push(obj.title);
+        });
+        console.log(items)
+      })
+    })
+    //getSearchWord(searchword);
   }else{
     response.send("Input the search word");
   }
 });
 
+/*
 async function getSearchWord(searchword){
   let requestOptions = {};
+  const items = [];
   await fetch(`https://api.apilayer.com/spoonacular/recipes/autocomplete?query=${searchword}&apikey=mGAW74xIyucj3kfBlC996NK7rw1pXg06`, requestOptions)
   .then(response => response.json())
   .then((data) =>{
     data.forEach(obj => {
-      Object.entries(obj).forEach(([key, value]) => {
-          console.log(`${key} ${value}`);
-      });
-      console.log('-------------------');
+      //obj now points to each entry in the data variable
+      items.push(obj.title);
     });
+    console.log(items)
   })
-  .catch((error) => {
+  .catch((error) => {5
     console.log(error)
   });
+}
+async function displaySearchResults(){
   
 }
+*/
